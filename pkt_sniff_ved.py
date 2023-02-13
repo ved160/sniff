33f873ba57fc028e3d69d3715d98fa14e52258ad
ved160 <125150743+ved160@users.noreply.github.com>
2023-02-13 14:07:12 -0500

    Update pkt_sniff_ved.py

--- pkt_sniff_ved.py
+++ pkt_sniff_ved.py
@@ -1,3 +1,5 @@
+import sys
+
 from scapy.all import *
 from scapy.layers.inet import IP
 from scapy.layers.http import HTTPRequest, TCP
@@ -7,13 +9,14 @@ import socket
 
 init()
 red = Fore.RED
+yellow = Fore.YELLOW
 blue = Fore.BLUE
 green = Fore.GREEN
 
 
 def sniff_packets(iface):
     if iface:
-        sniff(prn = process_pkt, iface = iface , store = False)
+        sniff(filter = 'dst port 80' ,prn = process_pkt, iface = iface , store = False)
     else:
         sniff(prn = process_pkt, store=False)
 
@@ -23,9 +26,21 @@ def process_pkt(pkt):
         dest_ip = pkt[IP].dst
         src_port = pkt[TCP].sport
         dest_port = pkt[TCP].dport
-        print(f"{green}Source IP: {src_ip}  port : {src_port} Destination IP: {dest_ip} port : {dest_port}")
+        print(f"{blue}Source IP: {src_ip}  port : {src_port} Destination IP: {dest_ip} port : {dest_port}")
+        
+        
+    if pkt.haslayer(HTTPRequest):
+        url = pkt[HTTPRequest].Host.decode() + pkt[HTTPRequest].Path.decode()
+        method = pkt[HTTPRequest].Method.decode()
+        print(f"{green} [+] {src_ip} is making HTTPrequest to {url} with method {method}")
+        print("[+] HTTP DATA :")
+        print(f"{yellow} {pkt[HTTPRequest].show()}")
+        
+        if pkt.haslayer(Raw):
+            print(f"{red} [=] useful raw data : {pkt.getlayer(Raw).load.decode()}")
 
-sniff_packets("eth0")
+iface = sys.argv[1]
+sniff_packets(iface)
 
 
 
